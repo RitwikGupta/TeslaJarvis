3ea3bad8e80e409140667e1470f909164900bff4
Eric Chen <eric.t.chen.17@dartmouth.edu>
2014-09-06 09:58:02 -0400

    added login to tesla program

---
+++ login.py
@@ -0,0 +1,19 @@
+# Eric Chen, MHacks IV, Sept 2014
+# Login to Tesla.
+
+from urllib2 import Request, urlopen
+from urllib import urlencode
+
+# Fake credentials
+login_dict = {
+	"email"   : "hello@tesla.com",
+	"password"   : "teslaRules",
+}
+values = urlencode(login_dict)
+headers = {"Content-Type": "application/x-www-form-urlencoded"}
+try:
+	request = Request("https://private-13a4ee565-timdorr.apiary-mock.com/login", data=values, headers=headers)
+	response_body = urlopen(request).read()
+	print response_body
+except:
+	print "failed to connect"

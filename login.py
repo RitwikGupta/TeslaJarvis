dc63850272bba7fe4f3cb57ddf80a01b39cfa89a
Eric Chen <eric.t.chen.17@dartmouth.edu>
2014-09-06 10:03:48 -0400

    put login program in a function

--- login.py
+++ login.py
@@ -4,16 +4,18 @@
 from urllib2 import Request, urlopen
 from urllib import urlencode
 
-# Fake credentials
-login_dict = {
-	"email"   : "hello@tesla.com",
-	"password"   : "teslaRules",
-}
-values = urlencode(login_dict)
-headers = {"Content-Type": "application/x-www-form-urlencoded"}
-try:
-	request = Request("https://private-13a4ee565-timdorr.apiary-mock.com/login", data=values, headers=headers)
-	response_body = urlopen(request).read()
-	print response_body
-except:
-	print "failed to connect"
+def login_tesla():
+	# Fake credentials
+	login_dict = {
+		"email"   : "hello@tesla.com",
+		"password"   : "teslaRules",
+	}
+	
+	values = urlencode(login_dict)
+	headers = {"Content-Type": "application/x-www-form-urlencoded"}
+	try:
+		request = Request("https://private-13a4ee565-timdorr.apiary-mock.com/login", data=values, headers=headers)
+		response_body = urlopen(request).read()
+		print response_body
+	except:
+		print "failed to connect"

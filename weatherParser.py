23b6475b46f53cb2f47190c938a0522b29595289
Eric Chen <eric.t.chen.17@dartmouth.edu>
2014-09-06 04:14:03 -0400

    weatherparser, which gets chance of rain given lat and long

---
+++ weatherParser.py
@@ -0,0 +1,21 @@
+# Eric Chen, MHacks IV, Sept 2014
+# TeslaJarvis - closes sunroof if rain on Tesla Model S
+# Returns probability of rain given a latitude and longitude.
+
+import requests
+
+def parseWeather(lat, lon):
+    url = "http://forecast.weather.gov/MapClick.php?lat=" + str(lat) + "&lon=" + str(lon)+"&FcstType=dwml"
+    response = requests.get(url)
+    probIndex = response.content.find('<probability-of-precipitation')
+    probEndIndex = response.content.find('/probability-of-precipitation>')
+    valueIndex = response.content.find('<value>', probIndex)
+    if probEndIndex < valueIndex:
+        return -1
+    length = len('<value>')
+    prob =  int(response.content[valueIndex + length:valueIndex+ length + 2])
+    print prob
+    return prob
+    
+
+parseWeather(42.46943, -83.51851943162569)

005482be04d2196c57a39ff91a30e0913b19b329
Eric Chen <eric.t.chen.17@dartmouth.edu>
2014-09-06 04:43:57 -0400

    added error cases for weatherParser

--- weatherParser.py
+++ weatherParser.py
@@ -5,13 +5,19 @@
 import requests
 
 def parseWeather(lat, lon):
-    url = "http://forecast.weather.gov/MapClick.php?lat=" + str(lat) + "&lon=" + str(lon)+"&FcstType=dwml"
-    response = requests.get(url)
+    url = "http://forecast.wdfweather.gov/MapClick.php?lat=" + str(lat) + "&lon=" + str(lon)+"&FcstType=dwml"
+    try:
+        response = requests.get(url)
+    except:
+        print "Url request ailed"
+        return "URL request failed"
     probIndex = response.content.find('<probability-of-precipitation')
+    if probIndex == -1:
+        return "No precipitation data"
     probEndIndex = response.content.find('/probability-of-precipitation>')
     valueIndex = response.content.find('<value>', probIndex)
     if probEndIndex < valueIndex:
-        return -1
+        return "No precipitation value found"
     length = len('<value>')
     prob =  int(response.content[valueIndex + length:valueIndex+ length + 2])
     print prob
